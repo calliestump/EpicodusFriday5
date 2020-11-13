cc0d6cddd288548a6c7fd0949f6d3bce8bd367d9
Callie Stump <callie@stu.mp>
2020-11-13 13:22:07 -0500

    passes earthAge() method

--- __tests__/method.test.js
+++ __tests__/method.test.js
@@ -4,6 +4,6 @@ import Calculator from "../src/method.js"
 describe("Calculator", () => {
   let testDay = new Calculator(2001,10,1);
   test("should check earthAge() functionality", () => {
-    expect(testDay.earthAge()).toEqual(18);
+    expect(testDay.earthAge()).toEqual(19);
   });
 });
